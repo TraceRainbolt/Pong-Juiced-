07274d48b042057c603b935d415d5ace3dc97b0a
TRACE-PC\Trace <tracerainbolt@gmail.com>
2017-05-17 19:57:11 -0700

    Initial commit.

---
+++ Assets/Scripts/BallBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviourScript : MonoBehaviour {
+    public float ymin, ymax;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(4, 4);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float yPos = transform.position.y;
+        if(yPos <= ymin || yPos >= ymax)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("hello");
+        rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+    }
+}
---
+++ Assets/Scripts/ComputerBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerBehaviourScript : MonoBehaviour {
+    public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector2(transform.position.x, ball.transform.position.y);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("here");
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+    }
+}
